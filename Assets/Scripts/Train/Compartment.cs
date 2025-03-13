a4a80322347a9a49ddee3594c2be6f8c29f846cb
bokob <bmc0520@naver.com>
2025-03-13 11:23:20 +0900

    [ADD] 간단한 주위 환경 프리팹 추가

---
+++ Assets/Scripts/Train/Compartment.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// 기차 칸
+/// </summary>
+public class Compartment : MonoBehaviour
+{
+    public Define.Compartment CompartmentType { get; protected set; }
+
+    public void Init()
+    {
+
+    }
+
+    public void Clear()
+    {
+
+    }
+}
--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -23,8 +23,18 @@ public class Define
     // 도구
     public enum Tool
     {
+        None,
         Axe,        // 도끼
         Pickaxe,    // 곡괭이
         Pail        // 양동이
+    }
+
+    // 열차 칸
+    public enum Compartment
+    {
+        None,
+        Engine,
+        WaterTank,
+
     }
 }
